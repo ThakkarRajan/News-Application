8ff5750a00cd6d2b3babf64fca1fbad930c67601
Rajan Thakkar <thakkarrajan10@gmail.com>
2024-06-20 00:30:03 -0400

    initial commit

---
+++ app/src/main/java/com/example/newsapplication/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.newsapplication
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.newsapplication.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        }
+    }
